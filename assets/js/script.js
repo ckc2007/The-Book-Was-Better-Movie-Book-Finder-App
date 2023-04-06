2c94f2fe560c44af9c63befa629e98aae5a3ab0f
ckc2007 <ckc2007@gmail.com>
2023-04-05 23:19:59 -0400

    fixed image search url

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,14 +30,22 @@ function getMovieList() {
         );
         // wrap the link code around the image code - append img to link
         movies.forEach((movie) => {
+          // console.log(movie.id);
+          // console.log(data);
           var liEl = document.createElement("li");
           var link = document.createElement("a");
           var imgEl = document.createElement("img");
+          var breakEl = document.createElement("br");
           link.textContent = movie.title;
+          const imagePath = movie.poster_path;
+          const posterUrl = `https://image.tmdb.org/t/p/w200/${imagePath}`;
           link.href = `https://www.themoviedb.org/movie/${movie.id}`;
-          imgEl.src = `https://api.themoivedb.org/3/movie/${movie.id}/images?api_key=${apiKeyMovieDB}`;
+          // fix here debug url
+          imgEl.src = posterUrl;
+          // imgEl.src = `https://api.themoivedb.org/3/movie/${movie.id}/images?api_key=${apiKeyMovieDB}`;
           imgEl.alt = `${movie.title}`;
           // append image to link/text
+          link.appendChild(breakEl);
           link.appendChild(imgEl);
           // append link and image to list element
           liEl.appendChild(link);
@@ -50,52 +58,58 @@ function getMovieList() {
 // this function gets the AUTHOR and call the getMovieList function for display
 // how?
 // is searches the movie db crew data for [job = 'novel'] << see api docs
-function getAuthor() {
-  searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    var movieTitle = movieTitleInput.value;
-    titlesArr.push(movieTitle);
-    // call the get movie list function so the list appears upon the search button being clicked
-    getMovieList();
-    // now do a fetch to get the name of the author
-    var url = `${baseMovieURL}?api_key=${apiKeyMovieDB}&query=${encodeURIComponent(
-      movieTitle
-    )}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        // not for feature - we can use the author to again search for related films that have that author on crew (implement later)
-        // take the first movie returned - this is usually the main one we want
-        if (data.results.length > 0) {
-          var movieId = data.results[0].id;
-          var creditsURL = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKeyMovieDB}`;
-          return fetch(creditsURL);
-        } else {
-          throw new Error(
-            `Sorry, can't find a movie with the title: "${movieTitle}"`
-          );
-        }
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        var author = data.crew.filter((person) => person.job === "Novel");
-        // clear the booklist
-        authorList.innerHTML = "";
-        if (author.length > 0) {
-          author.forEach((author) => {
-            var liEl = document.createElement("li");
-            liEl.textContent = author.name;
-            authorList.appendChild(liEl);
-          });
-        } else {
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  var movieTitle = movieTitleInput.value;
+  titlesArr.push(movieTitle);
+  // call the get movie list function so the list appears upon the search button being clicked
+  getMovieList();
+  // now do a fetch to get the name of the author
+  const urlAuth = `${searchMovieURL}?api_key=${apiKeyMovieDB}&query=${encodeURIComponent(
+    movieTitle
+  )}`;
+
+  fetch(urlAuth)
+    // debug here - ok fixed - was url issue above - search not base
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // not for feature - we can use the author to again search for related films that have that author on crew (implement later)
+      // take the first movie returned - this is usually the main one we want
+      if (data.results.length > 0) {
+        var movieId = data.results[0].id;
+        var creditsURL = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKeyMovieDB}`;
+        console.log(creditsURL);
+        return fetch(creditsURL);
+      } else {
+        throw new Error(
+          `Sorry, can't find a movie with the title: "${movieTitle}"`
+        );
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var author = data.crew.filter(
+        (person) =>
+          person.job === "Novel" ||
+          person.job === "Writer" ||
+          person.job === "Author"
+      );
+      // clear the booklist
+      authorList.innerHTML = "";
+      if (author.length > 0) {
+        author.forEach((author) => {
           var liEl = document.createElement("li");
-          liEl.textContent = "No author found";
+          liEl.textContent = author.name;
           authorList.appendChild(liEl);
-        }
-      });
-  });
-}
+        });
+      } else {
+        var liEl = document.createElement("li");
+        liEl.textContent = "No author found";
+        authorList.appendChild(liEl);
+      }
+    });
+});
 
 // Google Books API - will return a list of images of the books, blurb, author, and ISBN
